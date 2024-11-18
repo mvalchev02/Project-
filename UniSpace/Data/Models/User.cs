8d7082c0961b2cf196785593bdd18e730e939eea
Martin Valchev <stu2101261029@uni-plovdiv.bg>
2024-11-18 19:36:35 +0200

    User Entity

--- UniSpace/Data/Models/User.cs
+++ UniSpace/Data/Models/User.cs
@@ -1,6 +1,30 @@
-﻿namespace UniSpace.Data.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniSpace.Data.Models
 {
-    public class User
+    public class User : IdentityUser
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Role { get; set; }
+        [ForeignKey(nameof(Reservations))]
+        public virtual ICollection<Reservation> Reservations { get; set; }
     }
+
 }
