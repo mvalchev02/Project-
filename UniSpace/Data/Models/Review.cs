18bb8fbc8772a9e80e009a5e60304c8566dbfdc6
Martin Valchev <stu2101261029@uni-plovdiv.bg>
2024-11-18 19:52:22 +0200

    Review Entity

---
+++ UniSpace/Data/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using UniSpace.Data.Models;
+
+public class Review
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int RoomId { get; set; }
+    public Room Room { get; set; }
+    [Required]
+    public string UserId { get; set; }
+    public User User { get; set; }
+    [Required]
+
+    public int Rating { get; set; } 
+
+    public string Comment { get; set; }
+    [Required]
+    public DateTime CreatedDate { get; set; }
+}
