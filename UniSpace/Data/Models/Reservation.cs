5b48f05e523795716c4cda84270c30d5620d4c70
Martin Valchev <stu2101261029@uni-plovdiv.bg>
2024-11-18 19:47:33 +0200

    Room Entity

---
+++ UniSpace/Data/Models/Reservation.cs
@@ -0,0 +1,21 @@
+﻿namespace UniSpace.Data.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Status { get; set; }
+
+        public string Comments { get; set; }
+
+
+    }
+}
---
+++ UniSpace/Data/Models/Room.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniSpace.Data.Models
+{
+    public class Room
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+        [Required]
+        public int Capacity { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Location { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Equipment { get; set; }
+        [ForeignKey(nameof(Reservations))]
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
+
